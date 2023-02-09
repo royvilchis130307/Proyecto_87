248581af0b1f4f924eb40b1e1c9056142604bcd4
royvilchis130307 <120613710+royvilchis130307@users.noreply.github.com>
2023-02-08 20:29:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+/*
+    Usa la función beginPath() para empezar un path.
+    Luega, usa strokeStyle para establecer el color a "grey".
+    Usa lineWidth para establecer el ancho a 1.
+    Usa rect() para crear un rectángulo en 150 y 143
+con un ancho de 430 y una altura de 200.
+*/
+
+ctx._______;
+ctx._circle______ = "grey";
+ctx._______ = 1;
+ctx._______(x, y, width, height);
+ctx.stroke();
+
+/*
+    Usa la función beginPath() para empezar un path.
+    Luega, usa strokeStyle para establecer el color a "blue".
+    Usa lineWidth para establecer el ancho a 5.
+    Usa arc() para crear un círculo en x = 150, y = 143
+con un radio de 40, un startAngle de 0 y un endAngle de 2 * Math.PI.
+*/
+
+ctx._______;
+ctx._______ = "blue";
+ctx._______ = 5;
+ctx._______(x, y, r, startAngle, endAngle);
+ctx.stroke();
+
+// De igual forma, crea un círculo negro en la posición 350 y 210
+
+// De igual forma, crea un círculo rojo en la posición 210 y 40
+
+// De igual forma, crea un círculo naranja en la posición 300 y 250
+
+// De igual forma, crea un círculo verde en la posición 400 y 250
